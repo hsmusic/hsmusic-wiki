56298f4a826882abcf6f1065ebca7c6490f7e622
(quasar) nebula <towerofnix@gmail.com>
2020-12-09 17:37:23 -0400

    publish script

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This will pro8a8ly only work if you're....like, me!
+# So, don't even try, if you aren't.
+# 8ut you can tweak it to your own server if that's your vi8e.
+
+rsync -avhL site/ --info=progress2 nebula@ed1.club:/home/nebula/hsmusic
