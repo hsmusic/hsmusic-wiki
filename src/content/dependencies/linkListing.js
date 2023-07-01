2b1ffaa9696d38ed96a857f00ae2af3c13e7b08c
(quasar) nebula <qznebula@protonmail.com>
2023-07-01 14:16:12 -0300

    content: linkListing: use listing title (from stringsKey) by default

--- src/content/dependencies/linkListing.js
+++ src/content/dependencies/linkListing.js
@@ -1,8 +1,14 @@
 export default {
   contentDependencies: ['linkThing'],
+  extraDependencies: ['language'],
 
   relations: (relation, listing) =>
     ({link: relation('linkThing', 'localized.listing', listing)}),
 
-  generate: (relations) => relations.link,
+  data: (listing) =>
+    ({stringsKey: listing.stringsKey}),
+
+  generate: (data, relations, {language}) =>
+    relations.link
+      .slot('content', language.$(`listingPage.${data.stringsKey}.title`)),
 };
