e86f6a1c43277464b56ccfe1dfefaf393cd64f94
(quasar) nebula <qznebula@protonmail.com>
2023-10-02 19:07:34 -0300

    content: generateArtTagGalleryPage: misc. additions & updates

--- src/content/dependencies/generateArtTagGalleryPage.js
+++ src/content/dependencies/generateArtTagGalleryPage.js
@@ -1,4 +1,4 @@
-import {stitchArrays} from '#sugar';
+import {empty, stitchArrays, unique} from '#sugar';
 import {sortAlbumsTracksChronologically} from '#wiki-data';
 
 export default {
@@ -9,6 +9,7 @@ export default {
     'linkAlbum',
     'linkArtTag',
     'linkTrack',
+    'transformContent',
   ],
 
   extraDependencies: ['html', 'language', 'wikiData'],
@@ -20,14 +21,16 @@ export default {
   },
 
   query(sprawl, tag) {
-    const things = tag.taggedInThings.slice();
+    const directThings = tag.directlyTaggedInThings;
+    const indirectThings = tag.indirectlyTaggedInThings;
+    const allThings = unique([...directThings, ...indirectThings]);
 
-    sortAlbumsTracksChronologically(things, {
+    sortAlbumsTracksChronologically(allThings, {
       getDate: thing => thing.coverArtDate ?? thing.date,
       latestFirst: true,
     });
 
-    return {things};
+    return {directThings, indirectThings, allThings};
   },
 
   relations(relation, query, sprawl, tag) {
@@ -39,17 +42,38 @@ export default {
     relations.artTagMainLink =
       relation('linkArtTag', tag);
 
+    // TODO: linkArtTagInfo
+    relations.infoPageLink =
+      relation('linkArtTag', tag);
+
+    if (tag.descriptionShort) {
+      relations.description =
+        relation('transformContent', tag.descriptionShort);
+    }
+
+    if (!empty(tag.directAncestorTags)) {
+      relations.ancestorLinks =
+        tag.directAncestorTags.map(tag =>
+          relation('linkArtTag', tag));
+    }
+
+    if (!empty(tag.directDescendantTags)) {
+      relations.descendantLinks =
+        tag.directDescendantTags.map(tag =>
+          relation('linkArtTag', tag));
+    }
+
     relations.coverGrid =
       relation('generateCoverGrid');
 
     relations.links =
-      query.things.map(thing =>
+      query.allThings.map(thing =>
         (thing.album
           ? relation('linkTrack', thing)
           : relation('linkAlbum', thing)));
 
     relations.images =
-      query.things.map(thing =>
+      query.allThings.map(thing =>
         relation('image', thing.artTags));
 
     return relations;
@@ -62,22 +86,27 @@ export default {
 
     data.name = tag.name;
     data.color = tag.color;
+    data.hasLongerDescription = tag.descriptionShort !== tag.description;
 
-    data.numArtworks = query.things.length;
+    data.numArtworks = query.allThings.length;
 
     data.names =
-      query.things.map(thing => thing.name);
+      query.allThings.map(thing => thing.name);
 
     data.paths =
-      query.things.map(thing =>
+      query.allThings.map(thing =>
         (thing.album
           ? ['media.trackCover', thing.album.directory, thing.directory, thing.coverArtFileExtension]
           : ['media.albumCover', thing.directory, thing.coverArtFileExtension]));
 
     data.coverArtists =
-      query.things.map(thing =>
+      query.allThings.map(thing =>
         thing.coverArtistContribs
           .map(({who: artist}) => artist.name));
+
+    data.onlyFeaturedIndirectly =
+      query.allThings.map(thing =>
+        !query.directThings.includes(thing));
 
     return data;
   },
@@ -96,6 +125,20 @@ export default {
 
         mainClasses: ['top-index'],
         mainContent: [
+          html.tag('p', {class:' quick-info'},
+            {[html.joinChildren]: html.tag('br')},
+            {[html.onlyIfContent]: true},
+
+            [
+              relations.description?.slot('mode', 'inline'),
+              data.hasLongerDescription &&
+                language.$('tagPage.moreInfo', {
+                  link:
+                    relations.infoPageLink
+                      .slot('content', language.$('tagPage.moreInfo.link')),
+                }),
+            ]),
+
           html.tag('p', {class: 'quick-info'},
             language.$('tagPage.infoLine', {
               coverArts: language.countCoverArts(data.numArtworks, {
@@ -103,10 +146,27 @@ export default {
               }),
             })),
 
+          relations.ancestorLinks &&
+            html.tag('p', {class: 'quick-info'},
+              language.$('tagPage.descendsFrom', {
+                tags: language.formatConjunctionList(relations.ancestorLinks),
+              })),
+
+          relations.descendantLinks &&
+            html.tag('p', {clasS: 'quick-info'},
+              language.$('tagPage.desendants', {
+                tags: language.formatUnitList(relations.descendantLinks),
+              })),
+
           relations.coverGrid
             .slots({
               links: relations.links,
               names: data.names,
+
+              classes:
+                data.onlyFeaturedIndirectly.map(onlyFeaturedIndirectly =>
+                  (onlyFeaturedIndirectly ? 'featured-indirectly' : '')),
+
               images:
                 stitchArrays({
                   image: relations.images,
