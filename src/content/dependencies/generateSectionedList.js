12d399cfcf5600465660ec9b1d33553f4efea967
(quasar) nebula <qznebula@protonmail.com>
2023-12-06 22:05:52 -0400

    content: generateSectionedList: clean logic, sectionItemClasses slot

--- src/content/dependencies/generateSectionedList.js
+++ src/content/dependencies/generateSectionedList.js
@@ -4,39 +4,78 @@ export default {
   extraDependencies: ['html'],
 
   slots: {
-    class: {validate: v => v.oneOf(v.isString, v.sparseArrayOf(v.isString))},
-    sectionTitles: {validate: v => v.strictArrayOf(v.isHTML)},
-    sectionItems: {validate: v => v.strictArrayOf(v.isHTML)},
+    class: {
+      validate: v =>
+        v.oneOf(
+          v.isString,
+          v.sparseArrayOf(v.isString)),
+    },
+
+    sectionTitles: {
+      validate: v =>
+        v.strictArrayOf(v.isHTML),
+    },
+
+    sectionItems: {
+      validate: v =>
+        v.strictArrayOf(
+          v.strictArrayOf(v.isHTML)),
+    },
+
+    sectionItemClasses: {
+      validate: v =>
+        v.strictArrayOf(
+          v.strictArrayOf(
+            v.optional(v.isString))),
+    },
 
     collapseSections: {
       validate: v => v.is('bar', 'invisible'),
     },
   },
 
-  generate: (slots, {html}) =>
-    (slots.collapseSections === 'bar'
-      ? html.tag('ul', {class: slots.class},
-          slots.sectionItems.map((items, index, {length}) => [
-            items
-              .map(item => html.tag('li', item)),
-
-            index < length - 1 &&
-              html.tag('li', {role: 'separator'},
-                html.tag('hr')),
-          ]))
-   : slots.collapseSections === 'invisible'
-      ? html.tag('ul', {class: slots.class},
-          slots.sectionItems
-            .flat()
-            .map(item => html.tag('li', item)))
-      : html.tag('dl', {class: slots.class},
+  generate(slots, {html}) {
+    const listItems =
+      stitchArrays({
+        items: slots.sectionItems,
+        itemClasses:
+          slots.sectionItemClasses ??
+            Array.from({length: slots.sectionItems.length}, () => null),
+      }).map(({items, itemClasses}) =>
           stitchArrays({
-            title: slots.sectionTitles,
-            items: slots.sectionItems,
-          }).map(({title, items}) => [
-              html.tag('dt', title),
-              html.tag('dd',
-                html.tag('ul',
-                  items.map(item => html.tag('li', item)))),
-            ]))),
+            item: items,
+            itemClass:
+              itemClasses ??
+                Array.from({length: items.length}, () => null),
+          }).map(({item, itemClass}) =>
+              html.tag('li', {class: itemClass}, item)));
+
+    switch (slots.collapseSections) {
+      case 'bar':
+        return (
+          html.tag('ul', {class: slots.class},
+            listItems.map((items, index, {length}) => [
+              items,
+              index < length - 1 &&
+                html.tag('li', {role: 'separator'},
+                  html.tag('hr')),
+            ])));
+
+      case 'invisible':
+        return (
+          html.tag('ul', {class: slots.class}, listItems));
+
+      default:
+        return (
+          html.tag('dl', {class: slots.class},
+            stitchArrays({
+              title: slots.sectionTitles,
+              items: listItems,
+            }).map(({title, items}) => [
+                html.tag('dt', title),
+                html.tag('dd',
+                  html.tag('ul', items)),
+              ])));
+    }
+  },
 };
