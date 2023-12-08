043a6000a35f3c7fc7686f628d5fcc79b4054691
(quasar) nebula <qznebula@protonmail.com>
2023-12-08 18:54:43 -0400

    data: composite debug message tweaks

--- src/data/composite.js
+++ src/data/composite.js
@@ -1038,7 +1038,9 @@ export function compositeFrom(description) {
         (isBase
           ? ` (base):`
           : ` of ${steps.length}:`),
-        step]);
+        (step.flags?.compose
+          ? getCompositionName(step)
+          : step)]);
 
       const filterableDependencies = {
         ...availableDependencies,
@@ -1106,8 +1108,6 @@ export function compositeFrom(description) {
       debug(() => [
         `step #${i+1} - ${callingTransformForThisStep ? 'transform' : 'compute'}`,
         `with dependencies:`, filteredDependencies,
-        `selecting:`, selectDependencies,
-        `from available:`, filterableDependencies,
         ...callingTransformForThisStep ? [`from value:`, valueSoFar] : []]);
 
       let result;
