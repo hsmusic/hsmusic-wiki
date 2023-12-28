04b7afb850c22659322b549bb6a33ab76397193b
(quasar) nebula <qznebula@protonmail.com>
2023-12-28 16:16:52 -0400

    find: stub implementations for various incremental cache features

--- src/find.js
+++ src/find.js
@@ -4,6 +4,8 @@ import CacheableObject from '#cacheable-object';
 import {colors, logWarn} from '#cli';
 import {typeAppearance} from '#sugar';
 
+export const getMatchableNames = Symbol();
+
 function warnOrThrow(mode, message) {
   if (mode === 'error') {
     throw new Error(message);
@@ -16,8 +18,299 @@ function warnOrThrow(mode, message) {
   return null;
 }
 
+export class ReferenceMapping {
+  forwards = new Map();
+  backwards = new Map();
+
+  add(...args) {
+    if (args.length <= 1 || args.length >= 4) {
+      throw new Error(`Expected 2 or 3 arguments for add`);
+    }
+
+    return this.addEdge(args[0], args[1], args[2]);
+  }
+
+  addEdge(a, b, detail = null) {
+    this.#addDoubleNested(this.forwards, a, b, detail);
+    this.#addDoubleNested(this.backwards, b, a, detail);
+    return this;
+  }
+
+  #addDoubleNested(map, key, value, detail) {
+    if (!map.has(key)) {
+      const set = new Set([detail]);
+      const nested = new Map([[value, set]]);
+      map.set(key, nested);
+      return this;
+    }
+
+    const nested = map.get(key);
+
+    if (!nested.has(value)) {
+      const set = new Set([detail]);
+      nested.set(value, set);
+      return this;
+    }
+
+    nested.get(value).add(detail);
+    return this;
+  }
+
+  delete(...args) {
+    if (args.length === 1) {
+      return this.deleteNode(args[0]);
+    } else if (args.length === 2 || args.length === 3) {
+      return this.deleteEdge(args[0], args[1], args[2]);
+    } else {
+      throw new Error(`Expected 1, 2, or 3 arguments for delete`);
+    }
+  }
+
+  deleteNode(x) {
+    const forwardsCopy = new Set(this.forwards.get(x));
+    const backwardsCopy = new Set(this.backwards.get(x));
+
+    let deletedAnything = false;
+
+    for (const b of forwardsCopy) {
+      if (this.deleteEdge(x, b)) {
+        deletedAnything = true;
+      }
+    }
+
+    for (const a of backwardsCopy) {
+      if (this.deleteEdge(a, x)) {
+        deletedAnything = true;
+      }
+    }
+
+    return deletedAnything;
+  }
+
+  deleteEdge(a, b, detail = undefined) {
+    let deletedAnything = false;
+
+    if (detail === undefined) {
+      if (this.#deleteNested(this.forwards, a, b))
+        deletedAnything = true;
+      if (this.#deleteNested(this.backwards, b, a))
+        deletedAnything = true;
+    } else {
+      if (this.#deleteDoubleNested(this.forwards, a, b, detail))
+        deletedAnything = true;
+      if (this.#deleteDoubleNested(this.backwards, a, b, detail))
+        deletedAnything = true;
+    }
+
+    return deletedAnything;
+  }
+
+  #deleteNested(map, key, value) {
+    if (!map.has(key)) return false;
+
+    const nested = map.get(key);
+
+    if (!nested.has(value)) return false;
+
+    if (nested.size === 1) {
+      map.delete(key);
+    } else {
+      nested.delete(value);
+    }
+
+    return true;
+  }
+
+  #deleteDoubleNested(map, key, value, detail) {
+    if (!map.has(key)) return false;
+
+    const nested = map.get(key);
+
+    if (!nested.has(value)) return false;
+
+    const doubleNested = nested.get(value);
+
+    if (!doubleNested.has(detail)) return false;
+
+    if (doubleNested.size === 1 && nested.size === 1) {
+      map.delete(key);
+    } else if (doubleNested.size === 1) {
+      nested.delete(value);
+    } else {
+      doubleNested.delete(detail);
+    }
+
+    return true;
+  }
+
+  has(...args) {
+    if (args.length === 1) {
+      return this.hasNode(args[0]);
+    } else if (args.length === 2 || args.length === 3) {
+      return this.hasEdge(args[0], args[1], args[2]);
+    } else {
+      throw new Error(`Expected 1, 2, or 3 arguments for delete`);
+    }
+  }
+
+  hasNode(x) {
+    return this.forwards.has(x) || this.backwards.has(x);
+  }
+
+  hasEdge(a, b, detail = undefined) {
+    if (detail === undefined) {
+      return (
+        this.forwards.has(a) &&
+        this.forwards.get(a).has(b));
+    } else {
+      return (
+        this.forwards.has(a) &&
+        this.forwards.get(a).has(b) &&
+        this.forwards.get(a).get(b).has(detail));
+    }
+  }
+
+  get(x) {
+    return {
+      forwards: this.getForwards(x),
+      backwards: this.getBackwards(x),
+    };
+  }
+
+  getForwards(x) {
+    return this.forwards.get(x) ?? new Map();
+  }
+
+  getBackwards(x) {
+    return this.backwards.get(x) ?? new Map();
+  }
+}
+
+export class FindStore {
+  constructor() {
+    this.thingMapping = new ReferenceMapping();
+    this.metadataMapping = new ReferenceMapping();
+  }
+
+  getMatchableKeys(_thing) {
+    throw new Error(`getMatchableKeys not implemented`);
+  }
+
+  add(thing) {
+    const keys = this.getMatchableKeys(thing);
+
+    for (const key of keys) {
+      this.mapping.add(key, thing);
+    }
+  }
+
+  delete(thing) {
+    let deletedAnything = false;
+
+    if (this.mapping.delete(thing)) {
+      deletedAnything = true;
+    }
+
+    return deletedAnything;
+  }
+
+  get(key, {
+    mode = 'quiet',
+  } = {}) {
+    const goodMatch = this.goodMapping.getForwards(key);
+
+    if (goodMatch) {
+      return goodMatch;
+    }
+
+    const badMatch = this.badMapping.getForwards(key);
+
+    if (mode === 'error') {
+      if (badMatch) {
+        const goodKeys = this.goodMapping.getBackwards()
+        throw new WrongFindKeyError(key, );
+      }
+    }
+  }
+}
+
+export class FindByNameStore extends FindStore {
+  getMatchableKeys(thing) {
+    const getMatchableNamesFn = thing[getMatchableNames];
+
+    const nonNormalizedNames =
+      (getMatchableNamesFn
+        ? getMatchableNamesFn.apply(thing)
+        : [thing.name]);
+
+    const good = new Set();
+    const bad = new Set();
+
+    for (const nonNormalizedName of nonNormalizedNames) {
+      const normalizedName = this.#normalizeName(nonNormalizedName);
+
+      good.add(normalizedName);
+
+      if (normalizedName !== nonNormalizedName) {
+        bad.add(nonNormalizedName);
+      }
+    }
+
+    return {good, bad};
+  }
+
+  #normalizeName(name) {
+    return name.toLowerCase();
+  }
+}
+
+export class FindByDirectoryStore extends FindStore {
+  getMappingValues(thing) {
+    return [thing.directory];
+  }
+}
+
+export class CachedFind {
+  constructor({
+    referenceTypes = {},
+  } = {}) {
+    this.referenceTypes = referenceTypes;
+
+    this.byNameStore = new FindByNameStore();
+    this.byDirectoryStore = new FindByDirectoryStore();
+  }
+
+  addThing(thing) {
+    const {name, directory} = thing;
+
+    if (name in this.byName) {
+      this.byName[name].add(thing);
+    } else {
+      this.byName[name] = new Set([thing]);
+    }
+
+    if (directory in this.byDirectory) {
+      this.byDirectory[directory].add(thing);
+    } else {
+      this.byDirectory[directory] = new Set([thing]);
+    }
+  }
+
+  deleteThing(thing) {
+    const {name, directory} = thing;
+
+    if (name in this.byName) {
+      this.byName[name].delete(thing);
+    }
+
+    if (directory in this.byDirectory) {
+      this.byDirectory[directory].delete(thing);
+    }
+  }
+}
+
 export function processAllAvailableMatches(data, {
-  include = thing => true,
+  include = _thing => true,
 
   getMatchableNames = thing =>
     (Object.hasOwn(thing, 'name')
