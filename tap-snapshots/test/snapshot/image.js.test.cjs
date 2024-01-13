944ea96ae6f72c11527091a718b124d7e396cf34
(quasar) nebula <qznebula@protonmail.com>
2024-01-13 15:57:17 -0400

    test: update image snapshot tests

--- tap-snapshots/test/snapshot/image.js.test.cjs
+++ tap-snapshots/test/snapshot/image.js.test.cjs
@@ -7,36 +7,42 @@
 'use strict'
 exports[`test/snapshot/image.js > TAP > image (snapshot) > content warnings via tags 1`] = `
 <div class="reveal">
-    <div class="image-container"><div class="image-inner-area"><img src="media/album-art/beyond-canon/cover.png"></div></div>
-    <span class="reveal-text-container">
-        <span class="reveal-text">
-            cw: too cool for school
-            <br>
-            <span class="reveal-interaction">click to show</span>
-        </span>
-    </span>
+    <div class="image-container">
+        <div class="image-inner-area">
+            <img class="image" src="media/album-art/beyond-canon/cover.png">
+            <span class="reveal-text-container">
+                <span class="reveal-text">
+                    <img class="reveal-symbol" src="static/warning.svg?413">
+                    <br>
+                    <span class="reveal-warnings">too cool for school</span>
+                    <br>
+                    <span class="reveal-interaction">click to show</span>
+                </span>
+            </span>
+        </div>
+    </div>
 </div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > id with link 1`] = `
-<a class="box image-link" href="foobar" id="banana"><div class="image-container"><div class="image-inner-area"><img src="foobar"></div></div></a>
+<div class="image-container has-link"><a class="image-link" href="foobar" id="banana"><div class="image-inner-area"><img class="image" src="foobar"></div></a></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > id with square 1`] = `
-<div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img id="banana" src="foobar"></div></div></div></div>
+<div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img class="image" id="banana" src="foobar"></div></div></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > id without link 1`] = `
-<div class="image-container"><div class="image-inner-area"><img id="banana" src="foobar"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" id="banana" src="foobar"></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > lazy with square 1`] = `
-<noscript><div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img src="foobar"></div></div></div></div></noscript>
-<div class="square js-hide"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img class="lazy" data-original="foobar"></div></div></div></div>
+<noscript><div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img class="image" src="foobar"></div></div></div></div></noscript>
+<div class="square js-hide"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img class="image lazy" data-original="foobar"></div></div></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > link with file size 1`] = `
-<a class="box image-link" href="media/album-art/pingas/cover.png"><div class="image-container"><div class="image-inner-area"><img src="media/album-art/pingas/cover.png"></div></div></a>
+<div class="image-container has-link"><a class="image-link" href="media/album-art/pingas/cover.png"><div class="image-inner-area"><img class="image" src="media/album-art/pingas/cover.png"></div></a></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > missing image path 1`] = `
@@ -52,25 +58,25 @@ exports[`test/snapshot/image.js > TAP > image (snapshot) > source missing 1`] = 
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > source via path 1`] = `
-<div class="image-container"><div class="image-inner-area"><img src="media/album-art/beyond-canon/cover.png"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" src="media/album-art/beyond-canon/cover.png"></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > source via src 1`] = `
-<div class="image-container"><div class="image-inner-area"><img src="https://example.com/bananas.gif"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" src="https://example.com/bananas.gif"></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > square 1`] = `
-<div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img src="foobar"></div></div></div></div>
+<div class="square"><div class="square-content"><div class="image-container"><div class="image-inner-area"><img class="image" src="foobar"></div></div></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > thumb requested but source is gif 1`] = `
-<div class="image-container"><div class="image-inner-area"><img src="media/flash-art/5426.gif"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" src="media/flash-art/5426.gif"></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > thumbnail details 1`] = `
-<div class="image-container"><div class="image-inner-area"><img data-original-length="1200" data-thumbs="voluminous:1200 middling:900 petite:20" src="thumb/album-art/beyond-canon/cover.voluminous.jpg"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" data-original-length="1200" data-thumbs="voluminous:1200 middling:900 petite:20" src="thumb/album-art/beyond-canon/cover.voluminous.jpg"></div></div>
 `
 
 exports[`test/snapshot/image.js > TAP > image (snapshot) > width & height 1`] = `
-<div class="image-container"><div class="image-inner-area"><img width="600" height="400" src="foobar"></div></div>
+<div class="image-container"><div class="image-inner-area"><img class="image" width="600" height="400" src="foobar"></div></div>
 `
