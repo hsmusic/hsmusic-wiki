d91f879aee6cd624256be3cf5ddbcd638cc6c264
(quasar) nebula <qznebula@protonmail.com>
2024-01-13 21:40:10 -0400

    content: generateArtTagSidebar: hide sidebar without any details

--- src/content/dependencies/generateArtTagSidebar.js
+++ src/content/dependencies/generateArtTagSidebar.js
@@ -56,8 +56,15 @@ export default {
         .map(ancestorArtTag => ancestorArtTag.name),
   }),
 
-  generate: (data, relations, {html, language}) =>
-    relations.sidebar.slots({
+  generate(data, relations, {html, language}) {
+    if (
+      empty(relations.directDescendantArtTagLinks) &&
+      empty(relations.furthestAncestorArtTagMapLists)
+    ) {
+      return relations.sidebar;
+    }
+
+    return relations.sidebar.slots({
       boxes: [
         relations.sidebarBox.slots({
           content: [
@@ -98,5 +105,6 @@ export default {
           ],
         }),
       ],
-    }),
+    });
+  },
 };
