a4ae6e7c27f799d0878c432a399266e261dcc6a2
(quasar) nebula <qznebula@protonmail.com>
2024-02-24 13:15:11 -0400

    WIP

--- src/listing-spec/wiki/albums/by-duration.js
+++ src/listing-spec/wiki/albums/by-duration.js
@@ -1,3 +1,12 @@
+import {input} from '#composite';
+import {sortAlphabetically, sortByCount} from '#sort';
+import {filterByCount} from '#sugar';
+import {getTotalDuration} from '#wiki-data';
+
+import {exitWithoutDependency} from '#composite/control-flow';
+import {withMappedList, withPropertyFromList} from '#composite/data';
+import {withThingsSortedAlphabetically} from '#composite/wiki-data';
+
 export default {
   scope: 'wiki',
   directory: 'albums/by-duration',
@@ -5,4 +14,41 @@ export default {
 
   stringsKey: 'listAlbums.byDuration',
   contentFunction: 'listAlbumsByDuration',
+
+  data: () => [
+    exitWithoutDependency({
+      dependency: 'albumData',
+      value: input.value([]),
+    }),
+
+    withThingsSortedAlphabetically({
+      things: 'albumData',
+    }).outputs({
+      '#sortedThings': '#albums',
+    }),
+
+    withPropertyFromList({
+      list: '#albums',
+      property: input.value('tracks'),
+    }),
+
+    withMappedList({
+      list: '#albums.tracks',
+      map: input.value(getTotalDuration)
+    }).outputs({
+      '#mappedList': '#durations',
+    }),
+
+    {
+      dependencies: ['#albums', '#durations'],
+      compute: ({
+        ['#albums']: albums,
+        ['#durations']: durations,
+      }) => {
+        filterByCount(albums, durations);
+        sortByCount(albums, durations, {greatestFirst: true});
+        return {albums, durations};
+      },
+    },
+  ],
 };
