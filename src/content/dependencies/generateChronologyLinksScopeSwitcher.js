9b6bc1c9423dc0e355577b4922458c93114d5d89
(quasar) nebula <qznebula@protonmail.com>
2024-05-27 12:29:55 -0300

    content: generateChronologyLinksScopeSwitcher: manual blank detect

--- src/content/dependencies/generateChronologyLinksScopeSwitcher.js
+++ src/content/dependencies/generateChronologyLinksScopeSwitcher.js
@@ -18,38 +18,50 @@ export default {
     },
   },
 
-  generate: (slots, {html, language}) =>
-    html.tag('details', {class: 'scoped-chronology-switcher'},
-      slots.open &&
-        {open: true},
+  generate(slots, {html, language}) {
+    // TODO: Manual [html.onlyIfContent]-alike here is a bit unfortunate.
+    // We can't use a normal [html.onlyIfContent] because the summary counts
+    // as content - we'd need to encode that we want to exclude it from the
+    // content check (for the <details> element), somehow.
+    if (slots.contents.every(content => html.isBlank(content))) {
+      return html.blank();
+    }
 
-      [
-        html.tag('summary',
-          {class: 'underline-white'},
+    const summary =
+      html.tag('summary',
+        {class: 'underline-white'},
 
-          html.tag('span',
-            language.$('trackPage.nav.chronology.scope.title', {
-              scope:
-                slots.scopes.map((scope, index) =>
-                  html.tag('a', {class: 'switcher-link'},
-                    {href: '#'},
+        html.tag('span',
+          language.$('trackPage.nav.chronology.scope.title', {
+            scope:
+              slots.scopes.map((scope, index) =>
+                html.tag('a', {class: 'switcher-link'},
+                  {href: '#'},
 
-                    (index === 0
-                      ? {style: 'display: inline'}
-                      : {style: 'display: none'}),
+                  (index === 0
+                    ? {style: 'display: inline'}
+                    : {style: 'display: none'}),
 
-                    language.$('trackPage.nav.chronology.scope', scope))),
-            }))),
+                  language.$('trackPage.nav.chronology.scope', scope))),
+          })));
 
-        stitchArrays({
-          scope: slots.scopes,
-          content: slots.contents,
-        }).map(({scope, content}, index) =>
-            html.tag('div', {class: 'scope-' + scope},
-              (index === 0
-                ? {style: 'display: block'}
-                : {style: 'display: none'}),
+    const scopeContents =
+      stitchArrays({
+        scope: slots.scopes,
+        content: slots.contents,
+      }).map(({scope, content}, index) =>
+          html.tag('div', {class: 'scope-' + scope},
+            (index === 0
+              ? {style: 'display: block'}
+              : {style: 'display: none'}),
 
-              content)),
-      ]),
+            content));
+
+    return (
+      html.tag('details', {class: 'scoped-chronology-switcher'},
+        slots.open &&
+          {open: true},
+
+        [summary, scopeContents]));
+  },
 };
