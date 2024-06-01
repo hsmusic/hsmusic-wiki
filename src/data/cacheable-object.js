hsmusic/hsmusic-wiki

commit cebd7f4a32350f30c481b54d4777a6c910d5bcc1

data: CacheableObject: allow falsy default update values

src/data/cacheable-object.js
@@ -132,7 +132,7 @@ export default class CacheableObject {
         return;
       }
 
-      if (update?.default) {
+      if ('default' in update) {
         this[property] = update?.default;
       } else {
         this[property] = null;

commit 84954c24f11660d658e6bdd6ed20b15cd0fbaeb9

data: CacheableObject: check default update value more gracefully
Fixes a test failure introduced in cebd7f4a, and more generally,
crashes to do with flags: {update: true} but no update: {...}
configuration.

src/data/cacheable-object.js
@@ -132,7 +132,11 @@ export default class CacheableObject {
         return;
       }
 
-      if ('default' in update) {
+      if (
+        typeof update === 'object' &&
+        update !== null &&
+        'default' in update
+      ) {
         this[property] = update?.default;
       } else {
         this[property] = null;