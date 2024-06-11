cc053537f64c45b1ff3eb2f8952bab0a167786f4
(quasar) nebula <qznebula@protonmail.com>
2024-06-10 23:04:04 -0300

    content: generateListingPage: handle chunks w/o ids properly

--- src/content/dependencies/generateListingPage.js
+++ src/content/dependencies/generateListingPage.js
@@ -249,7 +249,7 @@ export default {
                   .clone()
                   .slots({
                     tag: 'dt',
-                    attributes: {id},
+                    attributes: [id && {id}],
 
                     title:
                       formatListingString({
