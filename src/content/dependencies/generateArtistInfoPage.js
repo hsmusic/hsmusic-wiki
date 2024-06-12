eb1bae12e2ea7021536909c43ce21f93691ccfbe
(quasar) nebula <qznebula@protonmail.com>
2024-06-12 18:16:31 -0300

    content: generateArtistInfoPage: content function syntax cleanup

--- src/content/dependencies/generateArtistInfoPage.js
+++ src/content/dependencies/generateArtistInfoPage.js
@@ -11,309 +11,289 @@ export default {
     'generateContentHeading',
     'generateCoverArtwork',
     'generatePageLayout',
-    'linkAlbum',
     'linkArtistGallery',
     'linkExternal',
-    'linkGroup',
-    'linkTrack',
     'transformContent',
   ],
 
-  extraDependencies: ['html', 'language', 'wikiData'],
+  extraDependencies: ['html', 'language'],
 
-  sprawl({wikiInfo}) {
-    return {
-      enableFlashesAndGames: wikiInfo.enableFlashesAndGames,
-    };
-  },
-
-  query(sprawl, artist) {
-    return {
-      // Even if an artist has served as both "artist" (compositional) and
-      // "contributor" (instruments, production, etc) on the same track, that
-      // track only counts as one unique contribution in the list.
-      allTracks:
-        unique(
-          ([
-            artist.trackArtistContributions,
-            artist.trackContributorContributions,
-          ]).flat()
-            .map(({thing}) => thing)),
-
-      // Artworks are different, though. We intentionally duplicate album data
-      // objects when the artist has contributed some combination of cover art,
-      // wallpaper, and banner - these each count as a unique contribution.
-      allArtworks:
+  query: (artist) => ({
+    // Even if an artist has served as both "artist" (compositional) and
+    // "contributor" (instruments, production, etc) on the same track, that
+    // track only counts as one unique contribution in the list.
+    allTracks:
+      unique(
         ([
-          artist.albumCoverArtistContributions,
-          artist.albumWallpaperArtistContributions,
-          artist.albumBannerArtistContributions,
-          artist.trackCoverArtistContributions,
+          artist.trackArtistContributions,
+          artist.trackContributorContributions,
         ]).flat()
-          .map(({thing}) => thing),
+          .map(({thing}) => thing)),
 
-      // Banners and wallpapers don't show up in the artist gallery page, only
-      // cover art.
-      hasGallery:
-        !empty(artist.albumCoverArtistContributions) ||
-        !empty(artist.trackCoverArtistContributions),
-    };
-  },
+    // Artworks are different, though. We intentionally duplicate album data
+    // objects when the artist has contributed some combination of cover art,
+    // wallpaper, and banner - these each count as a unique contribution.
+    allArtworks:
+      ([
+        artist.albumCoverArtistContributions,
+        artist.albumWallpaperArtistContributions,
+        artist.albumBannerArtistContributions,
+        artist.trackCoverArtistContributions,
+      ]).flat()
+        .map(({thing}) => thing),
 
-  relations(relation, query, sprawl, artist) {
-    const relations = {};
-    const sections = relations.sections = {};
+    // Banners and wallpapers don't show up in the artist gallery page, only
+    // cover art.
+    hasGallery:
+      !empty(artist.albumCoverArtistContributions) ||
+      !empty(artist.trackCoverArtistContributions),
+  }),
 
-    relations.layout =
-      relation('generatePageLayout');
+  relations: (relation, query, artist) => ({
+    layout:
+      relation('generatePageLayout'),
 
-    relations.artistNavLinks =
-      relation('generateArtistNavLinks', artist);
+    artistNavLinks:
+      relation('generateArtistNavLinks', artist),
 
-    if (artist.hasAvatar) {
-      relations.cover =
-        relation('generateCoverArtwork', []);
-    }
+    cover:
+      (artist.hasAvatar
+        ? relation('generateCoverArtwork', [])
+        : null),
 
-    if (artist.contextNotes) {
-      const contextNotes = sections.contextNotes = {};
-      contextNotes.content = relation('transformContent', artist.contextNotes);
-    }
+    contentHeading:
+      relation('generateContentHeading'),
 
-    if (!empty(artist.urls)) {
-      const visit = sections.visit = {};
-      visit.externalLinks =
-        artist.urls.map(url =>
-          relation('linkExternal', url));
-    }
+    contextNotes:
+      relation('transformContent', artist.contextNotes),
 
-    if (!empty(query.allTracks)) {
-      const tracks = sections.tracks = {};
-      tracks.heading = relation('generateContentHeading');
-      tracks.list = relation('generateArtistInfoPageTracksChunkedList', artist);
-      tracks.groupInfo = relation('generateArtistGroupContributionsInfo', query.allTracks);
-    }
+    visitLinks:
+      artist.urls
+        .map(url => relation('linkExternal', url)),
 
-    if (!empty(query.allArtworks)) {
-      const artworks = sections.artworks = {};
-      artworks.heading = relation('generateContentHeading');
-      artworks.list = relation('generateArtistInfoPageArtworksChunkedList', artist);
-      artworks.groupInfo =
-        relation('generateArtistGroupContributionsInfo', query.allArtworks);
+    tracksChunkedList:
+      relation('generateArtistInfoPageTracksChunkedList', artist),
 
-      if (query.hasGallery) {
-        artworks.artistGalleryLink =
-          relation('linkArtistGallery', artist);
-      }
-    }
+    tracksGroupInfo:
+      relation('generateArtistGroupContributionsInfo', query.allTracks),
 
-    if (sprawl.enableFlashesAndGames && !empty(artist.flashContributorContributions)) {
-      const flashes = sections.flashes = {};
-      flashes.heading = relation('generateContentHeading');
-      flashes.list = relation('generateArtistInfoPageFlashesChunkedList', artist);
-    }
+    artworksChunkedList:
+      relation('generateArtistInfoPageArtworksChunkedList', artist),
 
-    if (!empty(artist.albumsAsCommentator) || !empty(artist.tracksAsCommentator)) {
-      const commentary = sections.commentary = {};
-      commentary.heading = relation('generateContentHeading');
-      commentary.list = relation('generateArtistInfoPageCommentaryChunkedList', artist);
-    }
+    artworksGroupInfo:
+      relation('generateArtistGroupContributionsInfo', query.allArtworks),
 
-    return relations;
-  },
+    artistGalleryLink:
+      (query.hasGallery
+        ? relation('linkArtistGallery', artist)
+        : null),
 
-  data(query, sprawl, artist) {
-    const data = {};
+    flashesChunkedList:
+      relation('generateArtistInfoPageFlashesChunkedList', artist),
 
-    data.name = artist.name;
-    data.directory = artist.directory;
+    commentaryChunkedList:
+      relation('generateArtistInfoPageCommentaryChunkedList', artist),
+  }),
 
-    if (artist.hasAvatar) {
-      data.avatarFileExtension = artist.avatarFileExtension;
-    }
+  data: (query, artist) => ({
+    name:
+      artist.name,
 
-    data.totalTrackCount = query.allTracks.length;
-    data.totalDuration = artist.totalDuration;
+    directory:
+      artist.directory,
 
-    return data;
-  },
+    avatarFileExtension:
+      (artist.hasAvatar
+        ? artist.avatarFileExtension
+        : null),
 
-  generate(data, relations, {html, language}) {
-    const {sections: sec} = relations;
+    totalTrackCount:
+      query.allTracks.length,
 
-    return relations.layout
-      .slots({
-        title: data.name,
-        headingMode: 'sticky',
+    totalDuration:
+      artist.totalDuration,
+  }),
 
-        cover:
-          (relations.cover
-            ? relations.cover.slots({
-                path: [
-                  'media.artistAvatar',
-                  data.directory,
-                  data.avatarFileExtension,
-                ],
-              })
-            : null),
+  generate: (data, relations, {html, language}) =>
+    relations.layout.slots({
+      title: data.name,
+      headingMode: 'sticky',
 
-        mainContent: [
-          sec.contextNotes && [
+      cover:
+        (relations.cover
+          ? relations.cover.slots({
+              path: [
+                'media.artistAvatar',
+                data.directory,
+                data.avatarFileExtension,
+              ],
+            })
+          : null),
+
+      mainContent: [
+        html.tags([
+          html.tag('p',
+            {[html.onlyIfSiblings]: true},
+            language.$('releaseInfo.note')),
+
+          html.tag('blockquote',
+            {[html.onlyIfContent]: true},
+            relations.contextNotes),
+        ]),
+
+        html.tag('p',
+          {[html.onlyIfContent]: true},
+          language.$('releaseInfo.visitOn', {
+            [language.onlyIfOptions]: ['links'],
+            links:
+              language.formatDisjunctionList(
+                relations.visitLinks
+                  .map(link => link.slot('context', 'artist'))),
+          })),
+
+        html.tag('p',
+          {[html.onlyIfContent]: true},
+          language.$('artistPage.viewArtGallery', {
+            [language.onlyIfOptions]: ['link'],
+            link:
+              relations.artistGalleryLink?.slots({
+                content: language.$('artistPage.viewArtGallery.link'),
+              }),
+          })),
+
+        html.tag('p',
+          {[html.onlyIfContent]: true},
+          language.$('misc.jumpTo.withLinks', {
+            [language.onlyIfOptions]: ['links'],
+            links:
+              language.formatUnitList([
+                !html.isBlank(relations.tracksChunkedList) &&
+                  html.tag('a',
+                    {href: '#tracks'},
+                    language.$('artistPage.trackList.title')),
+
+                !html.isBlank(relations.artworksChunkedList) &&
+                  html.tag('a',
+                    {href: '#art'},
+                    language.$('artistPage.artList.title')),
+
+                !html.isBlank(relations.flashesChunkedList) &&
+                  html.tag('a',
+                    {href: '#flashes'},
+                    language.$('artistPage.flashList.title')),
+
+                !html.isBlank(relations.commentaryChunkedList) &&
+                  html.tag('a',
+                    {href: '#commentary'},
+                    language.$('artistPage.commentaryList.title')),
+              ].filter(Boolean)),
+          })),
+
+        html.tags([
+          relations.contentHeading.clone()
+            .slots({
+              tag: 'h2',
+              attributes: {id: 'tracks'},
+              title: language.$('artistPage.trackList.title'),
+            }),
+
+          data.totalDuration > 0 &&
             html.tag('p',
-              language.$('releaseInfo.note')),
-
-            html.tag('blockquote',
-              sec.contextNotes.content),
-          ],
-
-          sec.visit &&
-            html.tag('p',
-              language.$('releaseInfo.visitOn', {
-                links:
-                  language.formatDisjunctionList(
-                    sec.visit.externalLinks
-                      .map(link => link.slot('context', 'artist'))),
+              {[html.onlyIfSiblings]: true},
+              language.$('artistPage.contributedDurationLine', {
+                artist: data.name,
+                duration:
+                  language.formatDuration(data.totalDuration, {
+                    approximate: data.totalTrackCount > 1,
+                    unit: true,
+                  }),
               })),
 
-          sec.artworks?.artistGalleryLink &&
-            html.tag('p',
-              language.$('artistPage.viewArtGallery', {
-                link: sec.artworks.artistGalleryLink.slots({
+          relations.tracksChunkedList.slots({
+            groupInfo: [
+              relations.tracksGroupInfo
+                .clone()
+                .slots({
+                  title: language.$('artistPage.groupContributions.title.music'),
+                  showSortButton: true,
+                  sort: 'count',
+                  countUnit: 'tracks',
+                  visible: true,
+                }),
+
+              relations.tracksGroupInfo
+                .clone()
+                .slots({
+                  title: language.$('artistPage.groupContributions.title.music'),
+                  showSortButton: true,
+                  sort: 'duration',
+                  countUnit: 'tracks',
+                  visible: false,
+                }),
+            ],
+          }),
+        ]),
+
+        html.tags([
+          relations.contentHeading.clone()
+            .slots({
+              tag: 'h2',
+              attributes: {id: 'art'},
+              title: language.$('artistPage.artList.title'),
+            }),
+
+          html.tag('p',
+            {[html.onlyIfContent]: true},
+            language.$('artistPage.viewArtGallery.orBrowseList', {
+              [language.onlyIfOptions]: ['link'],
+              link:
+                relations.artistGalleryLink?.slots({
                   content: language.$('artistPage.viewArtGallery.link'),
                 }),
-              })),
+            })),
 
-          (sec.tracks || sec.artworsk || sec.flashes || sec.commentary) &&
-            html.tag('p',
-              language.$('misc.jumpTo.withLinks', {
-                links: language.formatUnitList(
-                  [
-                    sec.tracks &&
-                      html.tag('a',
-                        {href: '#tracks'},
-                        language.$('artistPage.trackList.title')),
-
-                    sec.artworks &&
-                      html.tag('a',
-                        {href: '#art'},
-                        language.$('artistPage.artList.title')),
-
-                    sec.flashes &&
-                      html.tag('a',
-                        {href: '#flashes'},
-                        language.$('artistPage.flashList.title')),
-
-                    sec.commentary &&
-                      html.tag('a',
-                        {href: '#commentary'},
-                        language.$('artistPage.commentaryList.title')),
-                  ].filter(Boolean)),
-              })),
-
-          sec.tracks && [
-            sec.tracks.heading
-              .slots({
-                tag: 'h2',
-                attributes: {id: 'tracks'},
-                title: language.$('artistPage.trackList.title'),
-              }),
-
-            data.totalDuration > 0 &&
-              html.tag('p',
-                language.$('artistPage.contributedDurationLine', {
-                  artist: data.name,
-                  duration:
-                    language.formatDuration(data.totalDuration, {
-                      approximate: data.totalTrackCount > 1,
-                      unit: true,
-                    }),
-                })),
-
-            sec.tracks.list
-              .slots({
-                groupInfo: [
-                  sec.tracks.groupInfo
-                    .clone()
-                    .slots({
-                      title: language.$('artistPage.groupContributions.title.music'),
-                      showSortButton: true,
-                      sort: 'count',
-                      countUnit: 'tracks',
-                      visible: true,
-                    }),
-
-                  sec.tracks.groupInfo
-                    .clone()
-                    .slots({
-                      title: language.$('artistPage.groupContributions.title.music'),
-                      showSortButton: true,
-                      sort: 'duration',
-                      countUnit: 'tracks',
-                      visible: false,
-                    }),
-                ],
-              }),
-          ],
-
-          sec.artworks && [
-            sec.artworks.heading
-              .slots({
-                tag: 'h2',
-                attributes: {id: 'art'},
-                title: language.$('artistPage.artList.title'),
-              }),
-
-            sec.artworks.artistGalleryLink &&
-              html.tag('p',
-                language.$('artistPage.viewArtGallery.orBrowseList', {
-                  link: sec.artworks.artistGalleryLink.slots({
-                    content: language.$('artistPage.viewArtGallery.link'),
-                  }),
-                })),
-
-            sec.artworks.list
-              .slots({
-                groupInfo:
-                  sec.artworks.groupInfo
-                    .slots({
-                      title: language.$('artistPage.groupContributions.title.artworks'),
-                      showBothColumns: false,
-                      sort: 'count',
-                      countUnit: 'artworks',
-                    }),
-              }),
-          ],
-
-          sec.flashes && [
-            sec.flashes.heading
-              .slots({
-                tag: 'h2',
-                attributes: {id: 'flashes'},
-                title: language.$('artistPage.flashList.title'),
-              }),
-
-            sec.flashes.list,
-          ],
-
-          sec.commentary && [
-            sec.commentary.heading
-              .slots({
-                tag: 'h2',
-                attributes: {id: 'commentary'},
-                title: language.$('artistPage.commentaryList.title'),
-              }),
-
-            sec.commentary.list,
-          ],
-        ],
-
-        navLinkStyle: 'hierarchical',
-        navLinks:
-          relations.artistNavLinks
+          relations.artworksChunkedList
             .slots({
-              showExtraLinks: true,
-            })
-            .content,
-      });
-  },
+              groupInfo:
+                relations.artworksGroupInfo
+                  .slots({
+                    title: language.$('artistPage.groupContributions.title.artworks'),
+                    showBothColumns: false,
+                    sort: 'count',
+                    countUnit: 'artworks',
+                  }),
+            }),
+        ]),
+
+        html.tags([
+          relations.contentHeading.clone()
+            .slots({
+              tag: 'h2',
+              attributes: {id: 'flashes'},
+              title: language.$('artistPage.flashList.title'),
+            }),
+
+          relations.flashesChunkedList,
+        ]),
+
+        html.tags([
+          relations.contentHeading.clone()
+            .slots({
+              tag: 'h2',
+              attributes: {id: 'commentary'},
+              title: language.$('artistPage.commentaryList.title'),
+            }),
+
+          relations.commentaryChunkedList,
+        ]),
+      ],
+
+      navLinkStyle: 'hierarchical',
+      navLinks:
+        relations.artistNavLinks
+          .slots({
+            showExtraLinks: true,
+          })
+          .content,
+    }),
 };
