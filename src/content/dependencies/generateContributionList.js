0902c43c7be3552ce2d61981a58f21791df5f45f
(quasar) nebula <qznebula@protonmail.com>
2024-06-12 13:34:53 -0300

    content: generateContributionList: blank if no contributions

--- src/content/dependencies/generateContributionList.js
+++ src/content/dependencies/generateContributionList.js
@@ -2,17 +2,20 @@ export default {
   contentDependencies: ['linkContribution'],
   extraDependencies: ['html'],
 
-  relations: (relation, contributions) =>
-    ({contributionLinks:
-        contributions
-          .map(contrib => relation('linkContribution', contrib))}),
+  relations: (relation, contributions) => ({
+    contributionLinks:
+      contributions
+        .map(contrib => relation('linkContribution', contrib)),
+  }),
 
   generate: (relations, {html}) =>
     html.tag('ul',
-      relations.contributionLinks.map(contributionLink =>
-        html.tag('li',
-          contributionLink
-            .slots({
+      {[html.onlyIfContent]: true},
+
+      relations.contributionLinks
+        .map(contributionLink =>
+          html.tag('li',
+            contributionLink.slots({
               showIcons: true,
               showContribution: true,
               preventWrapping: false,
