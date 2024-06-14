98ae7f44fb9a2b74ad7cdd25f75babe84fb4fc81
(quasar) nebula <qznebula@protonmail.com>
2024-06-13 23:39:35 -0300

    content: generateAdditionalFilesListChunk: nice capsule

--- src/content/dependencies/generateAdditionalFilesListChunk.js
+++ src/content/dependencies/generateAdditionalFilesListChunk.js
@@ -17,40 +17,31 @@ export default {
     },
   },
 
-  generate(slots, {html, language}) {
-    const capsule =
-      language.encapsulate('releaseInfo.additionalFiles.entry');
+  generate: (slots, {html, language}) =>
+    language.encapsulate('releaseInfo.additionalFiles.entry', capsule =>
+      html.tag('li',
+        html.tag('details',
+          html.isBlank(slots.items) &&
+            {open: true},
 
-    const summary =
-      html.tag('summary',
-        html.tag('span',
-          language.$(capsule, {
-            title:
-              html.tag('span', {class: 'group-name'},
-                slots.title),
-          })));
+          [
+            html.tag('summary',
+              html.tag('span',
+                language.$(capsule, {
+                  title:
+                    html.tag('span', {class: 'group-name'},
+                      slots.title),
+                }))),
 
-    const description =
-      html.tag('li', {class: 'entry-description'},
-        {[html.onlyIfContent]: true},
-        slots.description);
+            html.tag('ul', [
+              html.tag('li', {class: 'entry-description'},
+                {[html.onlyIfContent]: true},
+                slots.description),
 
-    const items =
-      (html.isBlank(slots.items)
-        ? html.tag('li',
-            language.$(capsule, 'noFilesAvailable'))
-        : slots.items);
-
-    const content =
-      html.tag('ul', [description, items]);
-
-    const details =
-      html.tag('details',
-        html.isBlank(slots.items) &&
-          {open: true},
-
-        [summary, content]);
-
-    return html.tag('li', details);
-  },
+              (html.isBlank(slots.items)
+                ? html.tag('li',
+                    language.$(capsule, 'noFilesAvailable'))
+                : slots.items),
+            ]),
+          ]))),
 };
