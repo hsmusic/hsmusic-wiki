3a0701f3c76da228d27da2140673620615004037
(quasar) nebula <qznebula@protonmail.com>
2024-06-17 21:16:07 -0300

    data: withContainingReverseContributionList

--- src/data/composite/things/contribution/index.js
+++ src/data/composite/things/contribution/index.js
@@ -1,6 +1,7 @@
 export {default as inheritFromContributionPresets} from './inheritFromContributionPresets.js';
 export {default as thingPropertyMatches} from './thingPropertyMatches.js';
 export {default as thingReferenceTypeMatches} from './thingReferenceTypeMatches.js';
+export {default as withContainingReverseContributionList} from './withContainingReverseContributionList.js';
 export {default as withContributionArtist} from './withContributionArtist.js';
 export {default as withContributionContext} from './withContributionContext.js';
 export {default as withMatchingContributionPresets} from './withMatchingContributionPresets.js';
