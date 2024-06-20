00e933a0c0a6d85459576e7536f586b07cb51377
(quasar) nebula <qznebula@protonmail.com>
2024-06-20 08:28:06 -0300

    content: generateGroupInfoPageAlbumsSection: match gallery sort

--- src/content/dependencies/generateGroupInfoPageAlbumsSection.js
+++ src/content/dependencies/generateGroupInfoPageAlbumsSection.js
@@ -1,3 +1,4 @@
+import {sortChronologically} from '#sort';
 import {empty, stitchArrays} from '#sugar';
 
 export default {
@@ -13,8 +14,15 @@ export default {
   extraDependencies: ['html', 'language'],
 
   query(group) {
+    // Typically, a latestFirst: false (default) chronological sort would be
+    // appropriate here, but navigation between adjacent albums in a group is a
+    // rather "essential" movement or relationship in the wiki, and we consider
+    // the sorting order of a group's gallery page (latestFirst: true) to be
+    // "canonical" in this regard. We exactly match its sort here, but reverse
+    // it, to still present earlier albums preceding later ones.
     const albums =
-      group.albums;
+      sortChronologically(group.albums.slice(), {latestFirst: true})
+        .reverse();
 
     const albumGroups =
       albums
