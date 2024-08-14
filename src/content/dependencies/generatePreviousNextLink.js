13e072363c7cef310aab39be3a52a134ef319dde
(quasar) nebula <qznebula@protonmail.com>
2024-08-14 13:03:39 -0300

    content, css, client: inert previous/next links

--- src/content/dependencies/generatePreviousNextLink.js
+++ src/content/dependencies/generatePreviousNextLink.js
@@ -15,21 +15,42 @@ export default {
       type: 'boolean',
       default: true,
     },
+
+    showWithoutLink: {
+      type: 'boolean',
+      default: true,
+    },
   },
 
-  generate: (slots, {html, language}) =>
-    (html.isBlank(slots.link) || !slots.direction
-      ? html.blank()
-      : slots.link.slots({
-          tooltipStyle: 'browser',
-          color: false,
+  generate(slots, {html, language}) {
+    if (!slots.direction) {
+      return html.blank();
+    }
 
-          attributes:
-            (slots.id
-              ? {id: `${slots.direction}-button`}
-              : null),
+    const attributes = html.attributes();
 
-          content:
-            language.$('misc.nav', slots.direction),
-        })),
+    if (slots.id) {
+      attributes.set('id', `${slots.direction}-button`);
+    }
+
+    if (html.isBlank(slots.link)) {
+      if (slots.showWithoutLink) {
+        return (
+          html.tag('a', {class: 'inert-previous-next-link'},
+            attributes,
+            language.$('misc.nav', slots.direction)));
+      } else {
+        return html.blank();
+      }
+    }
+
+    return slots.link.slots({
+      tooltipStyle: 'browser',
+      color: false,
+      attributes,
+
+      content:
+        language.$('misc.nav', slots.direction),
+    });
+  },
 };
