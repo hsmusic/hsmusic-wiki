99baab3a417d90a190095c628dd6d07d063eff71
(quasar) nebula <qznebula@protonmail.com>
2024-09-03 20:53:45 -0300

    content: gGIPAlbumsSection: stub divided-yearly albums list

--- src/content/dependencies/generateGroupInfoPageAlbumsSection.js
+++ src/content/dependencies/generateGroupInfoPageAlbumsSection.js
@@ -1,3 +1,4 @@
+import {chunkByCondition, stitchArrays} from '#sugar';
 import {sortChronologically} from '#sort';
 
 export default {
@@ -9,17 +10,32 @@ export default {
 
   extraDependencies: ['html', 'language'],
 
-  query: (group) => ({
+  query(group) {
     // Typically, a latestFirst: false (default) chronological sort would be
     // appropriate here, but navigation between adjacent albums in a group is a
     // rather "essential" movement or relationship in the wiki, and we consider
     // the sorting order of a group's gallery page (latestFirst: true) to be
     // "canonical" in this regard. We exactly match its sort here, but reverse
     // it, to still present earlier albums preceding later ones.
-    albums:
+    const albums =
       sortChronologically(group.albums.slice(), {latestFirst: true})
-        .reverse(),
-  }),
+        .reverse();
+
+    if (group.divideAlbumListsYearly) {
+      const albumsDividedYearly =
+        chunkByCondition(albums,
+          ({date: firstDate}, {date: secondDate}) =>
+            firstDate.getFullYear() !== secondDate.getFullYear());
+
+      const albumYears =
+        albumsDividedYearly
+          .map(([album]) => album.date.getFullYear());
+
+      return {albumsDividedYearly, albumYears};
+    }
+
+    return {albums};
+  },
 
   relations: (relation, query, group) => ({
     contentHeading:
@@ -29,13 +45,31 @@ export default {
       relation('linkGroupGallery', group),
 
     albumRows:
-      query.albums.map(album =>
-        relation('generateGroupInfoPageAlbumsSectionAlbumRow',
-          album,
-          group)),
+      (query.albums
+        ? query.albums
+            .map(album =>
+              relation('generateGroupInfoPageAlbumsSectionAlbumRow',
+                album,
+                group))
+        : []),
+
+    albumRowsDividedYearly:
+      (query.albumsDividedYearly
+        ? query.albumsDividedYearly
+            .map(albums => albums
+              .map(album =>
+                relation('generateGroupInfoPageAlbumsSectionAlbumRow',
+                  album,
+                  group)))
+        : []),
   }),
 
-  generate: (relations, {html, language}) =>
+  data: (query) => ({
+    albumDividedYears:
+      query.albumYears ?? [],
+  }),
+
+  generate: (data, relations, {html, language}) =>
     language.encapsulate('groupInfoPage', pageCapsule =>
       language.encapsulate(pageCapsule, 'albumList', listCapsule =>
         html.tags([
@@ -59,5 +93,17 @@ export default {
             {[html.onlyIfContent]: true},
 
             relations.albumRows),
+
+          stitchArrays({
+            year: data.albumDividedYears,
+            albumRows: relations.albumRowsDividedYearly,
+          }).map(({year, albumRows}) => [
+              html.tag('h3', year),
+              html.tag('ul',
+                albumRows.map(albumRow =>
+                  albumRow.slots({
+                    showDatetimestamp: false,
+                  }))),
+            ]),
         ]))),
 };
