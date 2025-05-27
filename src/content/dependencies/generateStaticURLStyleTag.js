792548d5115e65b97b161781f325adfe268f8a28
(quasar) nebula <qznebula@protonmail.com>
2025-05-27 18:48:22 -0300

    content: generateStaticURLStyleTag: use generateStyleTag

--- src/content/dependencies/generateStaticURLStyleTag.js
+++ src/content/dependencies/generateStaticURLStyleTag.js
@@ -1,9 +1,23 @@
 export default {
-  extraDependencies: ['html', 'to'],
+  contentDependencies: ['generateStyleTag'],
+  extraDependencies: ['to'],
 
-  generate: ({html, to}) =>
-    html.tag('style', {class: 'static-url-style'},
-      `.image-media-link::after {\n` +
-      `    mask-image: url("${to('staticMisc.path', 'image.svg')}");\n` +
-      `}`),
+  relations: (relation) => ({
+    styleTag:
+      relation('generateStyleTag'),
+  }),
+
+  generate: (relations, {to}) =>
+    relations.styleTag.slots({
+      attributes: {class: 'static-url-style'},
+
+      rules: [
+        {
+          select: '.image-media-link::after',
+          declare: [
+            `mask-image: url("${to('staticMisc.path', 'image.svg')}");`
+          ],
+        },
+      ],
+    }),
 };
