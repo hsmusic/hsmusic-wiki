65c6030ae6056bf999844c62b5fef6d0848d2631
(quasar) nebula <qznebula@protonmail.com>
2025-08-23 17:59:38 -0300

    data: WikiDate: toString, custom inspect, zero time values

--- src/data/things/wiki-date.js
+++ src/data/things/wiki-date.js
@@ -1,3 +1,5 @@
+import {inspect} from 'node:util';
+
 import Thing from '#thing';
 import {isInteger} from '#validators';
 
@@ -13,13 +15,6 @@ function range(a, b) {
   };
 }
 
-function numberDescriptor() {
-  return {
-    flags: {update: true, expose: true},
-    update: {validate: isInteger},
-  };
-}
-
 function rangeDescriptor(a, b) {
   return {
     flags: {update: true, expose: true},
@@ -27,17 +22,35 @@ function rangeDescriptor(a, b) {
   };
 }
 
+function nullableRangeDescriptor(a, b) {
+  return rangeDescriptor(a, b);
+}
+
+function zeroedRangeDescriptor(a, b) {
+  return {
+    ...rangeDescriptor(a, b),
+    expose: {
+      transform: value => value ?? 0,
+    },
+  };
+}
+
 export class WikiDate extends Thing {
   static [Thing.friendlyName] = `Wiki Date`;
 
   static [Thing.getPropertyDescriptors] = () => ({
-    year: numberDescriptor(),
-    month: rangeDescriptor(0, 11),
-    day: rangeDescriptor(1, 31),
-    hour: rangeDescriptor(0, 23),
-    minute: rangeDescriptor(0, 59),
-    second: rangeDescriptor(0, 59),
-    millisecond: rangeDescriptor(0, 999),
+    year: {
+      flags: {update: true, expose: true},
+      update: {validate: isInteger},
+    },
+
+    month: nullableRangeDescriptor(0, 11),
+    day: nullableRangeDescriptor(1, 31),
+
+    hour: zeroedRangeDescriptor(0, 23),
+    minute: zeroedRangeDescriptor(0, 59),
+    second: zeroedRangeDescriptor(0, 59),
+    millisecond: zeroedRangeDescriptor(0, 999),
   });
 
   static [Thing.yamlDocumentSpec] = {
@@ -89,5 +102,88 @@ export class WikiDate extends Thing {
       this.millisecond ?? 0);
 
     return date;
+  }
+
+  toString() {
+    const parts = [];
+
+    // sigh
+    const months =
+     ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+
+    const has = prop => this[prop] !== null;
+
+    const monthDay =
+      (has('month') && has('day')
+        ? `${months[this.month]} ${this.day}`
+     : has('month')
+        ? `${months[this.month]}`
+     : has('day')
+        ? `Day ${this.day} of no month`
+        : null);
+
+    const year =
+      (has('year')
+        ? `${this.year}`
+        : null);
+
+    if (monthDay) {
+      parts.push(monthDay);
+    }
+
+    if (monthDay && year) {
+      if (has('day')) {
+        parts.push(', ');
+      } else {
+        parts.push(' ');
+      }
+    }
+
+    if (year) {
+      parts.push(year);
+    }
+
+    const pad = (value, length) =>
+      value.toString().padStart(length, '0');
+
+    const millisecond =
+      (this.millisecond
+        ? `.${pad(this.millisecond, 3)}`
+        : '');
+
+    const second =
+      (this.second || this.millisecond
+        ? `:${pad(this.second, 2)}`
+        : '');
+
+    const hourMinute =
+      (this.hour || this.minute || this.second || this.millisecond
+        ? `${pad(this.hour, 2)}:${pad(this.minute, 2)}`
+        : '');
+
+    const time = hourMinute + second + millisecond;
+
+    if (monthDay && time) {
+      parts.push(', ');
+    } else if (year && time) {
+      parts.push(' ');
+    }
+
+    if (time) {
+      parts.push(time);
+    }
+
+    return parts.join('');
+  }
+
+  [inspect.custom]() {
+    const parts = [];
+
+    parts.push(Thing.prototype[inspect.custom].apply(this));
+    parts.push(': ');
+    parts.push(this.toString());
+
+    return parts.join('');
   }
 }
