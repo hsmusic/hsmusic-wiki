28232be4352a3b67e8c60df294c9686ad9160640
(quasar) nebula <qznebula@protonmail.com>
2026-02-01 00:08:53 -0400

    content: generateMusicVideoArtistsLine: proper dynamics

--- src/content/dependencies/generateMusicVideoArtistsLine.js
+++ src/content/dependencies/generateMusicVideoArtistsLine.js
@@ -6,34 +6,46 @@ export default {
 
   data: (musicVideo) => ({
     label:
-      (musicVideo.label !== 'Music video'
-        ? musicVideo.label
-        : null),
+      musicVideo.label,
   }),
 
   generate: (data, relations, {html, language}) =>
     language.encapsulate('misc.musicVideo.artistsLine', artistsLineCapsule =>
       language.encapsulate(artistsLineCapsule, workingCapsule => {
-        const workingOptions = {[language.onlyIfOptions]: ['credit']};
+        const credit = relations.artistCredit;
 
-        if (data.label) {
+        credit.setSlots({
+          normalStringKey:
+            language.encapsulate(artistsLineCapsule, 'credit'),
+
+          showAnnotation: true,
+          showChronology: true,
+
+          chronologyKind: 'musicVideo',
+        });
+
+        if (html.isBlank(credit)) {
+          return html.blank();
+        }
+
+        if (data.label === 'Music video' || !data.label) {
+          credit.setSlots({
+            normalStringKey:
+              language.encapsulate(artistsLineCapsule, 'noLabel'),
+          });
+
+          return credit;
+        }
+
+        const workingOptions = {};
+
+        workingOptions.credit =
+          html.tag('span', {class: 'by'}, credit);
+
+        if (data.label && data.label !== 'Music video') {
           workingCapsule += '.customLabel';
           workingOptions.label = data.label;
         }
-
-        workingOptions.credit =
-          html.tag('span', {class: 'by'},
-            {[html.onlyIfContent]: true},
-
-            relations.artistCredit.slots({
-              normalStringKey:
-                language.encapsulate(artistsLineCapsule, 'credit'),
-
-              showAnnotation: true,
-              showChronology: true,
-
-              chronologyKind: 'musicVideo',
-            }));
 
         return language.$(workingCapsule, workingOptions);
       })),
