7fc9960d4bebe5bc3b02314d10b504df89b9e9d3
(quasar) nebula <qznebula@protonmail.com>
2026-03-25 10:05:52 -0300

    content, css: generateTrackListDividedByGroups: division-list

--- src/content/dependencies/generateTrackListDividedByGroups.js
+++ src/content/dependencies/generateTrackListDividedByGroups.js
@@ -80,7 +80,7 @@ export default {
   generate: (data, relations, slots, {html, language}) =>
     relations.flatList ??
 
-    html.tag('dl',
+    html.tag('dl', {class: 'division-list'},
       {[html.onlyIfContent]: true},
 
       language.encapsulate('trackList', listCapsule => [
