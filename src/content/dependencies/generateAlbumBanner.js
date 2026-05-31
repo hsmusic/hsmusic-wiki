90a3498be45578e8d7da7a01eaa9b90dc3aeb533
(quasar) nebula <qznebula@protonmail.com>
2026-05-31 12:39:19 -0300

    content: generateAlbumBanner: path

--- src/content/dependencies/generateAlbumBanner.js
+++ src/content/dependencies/generateAlbumBanner.js
@@ -19,8 +19,8 @@ export default {
     }
 
     return {
-      path: ['media.albumBanner', album.directory, album.bannerFileExtension],
-      dimensions: album.bannerDimensions,
+      path: album.bannerArtwork.path,
+      dimensions: album.bannerArtwork.dimensions,
     };
   },
 
